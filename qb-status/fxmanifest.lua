093ae121eb6504387c6e91aed886e5066ecac080
Harley <83428883+Harley2787@users.noreply.github.com>
2022-04-07 21:03:10 +0100

    New Script: qb-status
    [WIP] May be bugs! Adding more soon feel free to pop requests in! :)

---
+++ qb-status/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+
+name "QB Status"
+version '1.0.0'
+description "Made By Harley: Status Of The Server!"
+author "NeroHiro"
+
+ui_page "./ui/index.html"
+
+files{
+    ".client/main.lua"
+}
+
+client_script "client/main.lua"
